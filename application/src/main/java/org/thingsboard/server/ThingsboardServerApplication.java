adfeca4164f206c766224c0017f59660ed96fe12
N.A.N.N.N. Arachchi <158262381+Nethnissanka@users.noreply.github.com>
2025-06-23 15:12:17 +0530

    Update ThingsboardServerApplication.java

--- application/src/main/java/org/thingsboard/server/ThingsboardServerApplication.java
+++ application/src/main/java/org/thingsboard/server/ThingsboardServerApplication.java
@@ -13,6 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+
+//new comment
 package org.thingsboard.server;
 
 import lombok.extern.slf4j.Slf4j;
