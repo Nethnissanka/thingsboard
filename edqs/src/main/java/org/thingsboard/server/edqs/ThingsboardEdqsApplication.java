ce102ba8f5db8dfaec81e6a52eee6b6e368dc231
N.A.N.N.N. Arachchi <158262381+Nethnissanka@users.noreply.github.com>
2025-06-24 13:14:56 +0530

    Update ThingsboardEdqsApplication.java

--- edqs/src/main/java/org/thingsboard/server/edqs/ThingsboardEdqsApplication.java
+++ edqs/src/main/java/org/thingsboard/server/edqs/ThingsboardEdqsApplication.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// new comment...
 package org.thingsboard.server.edqs;
 
 import lombok.extern.slf4j.Slf4j;
