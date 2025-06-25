1cce155ed4ede016c9f90398ad1eb4ec4ae53e5a
N.A.N.N.N. Arachchi <158262381+Nethnissanka@users.noreply.github.com>
2025-06-25 16:04:52 +0530

    Update Dao.java

--- dao/src/main/java/org/thingsboard/server/dao/Dao.java
+++ dao/src/main/java/org/thingsboard/server/dao/Dao.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// jenkins pipeline testing
 package org.thingsboard.server.dao;
 
 import com.google.common.util.concurrent.ListenableFuture;
