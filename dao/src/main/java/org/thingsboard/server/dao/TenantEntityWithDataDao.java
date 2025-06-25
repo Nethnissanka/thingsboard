563725eeb96e6f32aadb8084adb244c9efd6de68
N.A.N.N.N. Arachchi <158262381+Nethnissanka@users.noreply.github.com>
2025-06-25 12:06:19 +0530

    Update TenantEntityWithDataDao.java

--- dao/src/main/java/org/thingsboard/server/dao/TenantEntityWithDataDao.java
+++ dao/src/main/java/org/thingsboard/server/dao/TenantEntityWithDataDao.java
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// test comment to check pipeline in jenkins
 package org.thingsboard.server.dao;
 
 import org.thingsboard.server.common.data.id.TenantId;
